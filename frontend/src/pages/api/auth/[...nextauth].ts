0129f7f6ad5ef5bf33391232ef5b42e8d72e4661
Karteek <ati.karteek99@gmail.com>
2023-07-12 18:19:22 +0530

    cookie name update

--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -19,6 +19,17 @@ export const authOptions: NextAuthOptions = {
       return { ...session, user: { ...session.user, ...user } };
     },
   },
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+      },
+    },
+  },
 };
 
 export default NextAuth(authOptions);
