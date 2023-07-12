6d301e99443c963ec7278533dc029e322d291f52
Karteek <ati.karteek99@gmail.com>
2023-07-12 16:30:44 +0530

    minor bugs

--- backend/src/util/getServerSession.ts
+++ backend/src/util/getServerSession.ts
@@ -4,6 +4,8 @@ export const getServerSession = async (cookie: string) => {
   const res = await fetch(`${process.env.CLIENT_ORIGIN}/api/auth/session`, {
     headers: { cookie: cookie },
   });
+  console.log(`${process.env.CLIENT_ORIGIN}/api/auth/session`);
+  console.log(res);
   const session = await res.json();
   return session;
 };
