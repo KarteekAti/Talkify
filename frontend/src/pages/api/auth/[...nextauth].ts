43b5f9e7d8048392a6ff17f53fcc7c58c59a2552
Karteek <ati.karteek99@gmail.com>
2023-07-12 13:26:44 +0530

    secret key

--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -13,7 +13,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET as string,
+  secret: "asbidf2342!@$%39",
   callbacks: {
     async session({ session, user, token }) {
       return { ...session, user: { ...session.user, ...user } };
