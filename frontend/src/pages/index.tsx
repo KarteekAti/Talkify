0ff9723d3d94301852eb3203a6de65efef925dfa
Karteek <ati.karteek99@gmail.com>
2023-07-12 18:36:49 +0530

    cookies

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,6 +8,7 @@ import { authOptions } from "./api/auth/[...nextauth]";
 
 const Index = () => {
   const { data: session } = useSession();
+  console.log(document.cookie.split(";"));
   const reloadSession = () => {
     const event = new Event("visibilitychange");
     document.dispatchEvent(event);
@@ -26,8 +27,6 @@ const Index = () => {
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await getServerSession(context.req, context.res, authOptions);
-  const cookie = context.req.cookies["__Secure-next-auth.session-token"];
-  localStorage.setItem("token", cookie);
   return {
     props: {
       session,
